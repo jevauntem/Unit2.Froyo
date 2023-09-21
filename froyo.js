57dbaf95ccea8954a222c21a7b4be6b42f9b7b28
Your Name <jevauntem@gmail.com>
2023-09-21 16:17:32 -0400

    added object, key and value

---
+++ froyo.js
@@ -0,0 +1,18 @@
+const userInput = prompt (
+    "Enter a list of froyo flavors, seperate each with commas.",
+    "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
+    );
+ //dialogue box with string asking for flavors
+ //user input box shows flavors seperated with commas
+
+ const froyo={
+    vanilla: 3,
+    coffee: 2,
+    strawberry: 1,
+ }
+ // index of flavors and value of amount ordered
+
+ console.table(froyo)
+ //table in console shows flavors and value(amount ordered)
+
+ 
