b55f0bd1e61d50635b95d56033ebf14ba986f1ed
Your Name <jevauntem@gmail.com>
2023-09-21 21:05:14 -0400

    fin.

--- froyo.js
+++ froyo.js
@@ -2,17 +2,26 @@ const userInput = prompt (
     "Enter a list of froyo flavors, seperate each with commas.",
     "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
     );
+    console.log(userInput);
  //dialogue box with string asking for flavors
  //user input box shows flavors seperated with commas
 
- const froyo={
-    vanilla: 3,
-    coffee: 2,
-    strawberry: 1,
- }
- // index of flavors and value of amount ordered
+ const flavorString = userInput.split(',');
+console.log(flavorString);
+ // index flavors of froyo
 
- console.table(froyo)
+ const flavorCounts= {};
+ for (const item of flavorString) {
+    if (flavorCounts[item]){
+        flavorCounts[item]++;
+    } else {
+        flavorCounts[item] = 1;
+    }
+ }
+ console.log(flavorCounts);
+//log value of each key(flavor ordered)
+ 
+console.table(flavorCounts);
  //table in console shows flavors and value(amount ordered)
 
  
